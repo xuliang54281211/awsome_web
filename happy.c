29e06442c5b8d1af4f966a27ef0942dcb2ca5d04
root <root@localhost.localdomain>
2018-05-13 22:52:39 -0700

    first

---
+++ happy.c
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: happy.c
+	> Author: DJM
+	> Mail: ollydbgkernel@sina.cn 
+	> Created Time: Sun 13 May 2018 07:07:07 PM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+
